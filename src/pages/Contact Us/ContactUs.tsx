b75e61dfc14ec37a34e118405670de5596d8f044
Sanjay-mobrilz <sanjay@mobrilz.agency>
2025-01-24 15:27:04 +0530

    update_contact_pages

--- src/pages/Contact Us/ContactUs.tsx
+++ src/pages/Contact Us/ContactUs.tsx
@@ -1,11 +1,5 @@
 // import React from "react";
-import {
-  FaFacebook,
-  FaInstagram,
- 
-  FaPhoneAlt,
-  FaTwitter,
-} from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaPhoneAlt, FaTwitter } from "react-icons/fa";
 import Layout from "../../UI/Layout/Layout";
 
 import { HiLocationMarker } from "react-icons/hi";
@@ -15,8 +9,8 @@ import { MdEmail } from "react-icons/md";
 const ContactUs = () => {
   return (
     <Layout>
-      <div className="w-full h-3/4 bg-blue-400 flex justify-around gap-4 py-3">
-        <div>
+      {/* <div className="flex items-center justify-center w-full h-screen bg-blue-400 py-3">
+        <div className="">
           <h3 className="text-6xl text-white font-bold">
             We're Here to <br /> Help!
           </h3>
@@ -135,6 +129,118 @@ const ContactUs = () => {
               </button>
             </div>
           </form>
+        </div>
+      </div> */}
+      <div className=" flex justify-center content-center">
+        <div className="w-3/4 h-3/4 py-3 bg-blue-400 flex justify-center gap-4 items-center">
+          <div className="flex flex-col items-start w-1/2 space-y-6">
+            <h3 className="text-6xl text-white font-bold">
+              We're Here to <br /> Help!
+            </h3>
+            <p className="my-4 text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+              Commodi tempora provident, minus id quaerat aliquid quisquam
+              deleniti at ullam praesentium beatae.
+            </p>
+            <div>
+              <ul className="flex flex-col space-y-4">
+                <li className="flex items-center text-xl text-white">
+                  <FaPhoneAlt className="mr-2" />
+                  <span>+91 8756321231</span>
+                </li>
+                <li className="flex items-center text-xl text-white">
+                  <MdEmail className="mr-2" />
+                  <span>Noida564@gmail.com</span>
+                </li>
+                <li className="flex items-center text-xl text-white">
+                  <HiLocationMarker className="mr-2" />
+                  <span>Noida sec 63 UP</span>
+                </li>
+              </ul>
+            </div>
+            <div className="mt-16">
+              <ul className="flex justify-center">
+                <li>
+                  <FaFacebook className="text-3xl mx-2 text-white" />
+                </li>
+                <li>
+                  <FaTwitter className="text-3xl mx-2 text-white" />
+                </li>
+                <li>
+                  <FaInstagram className="text-3xl mx-2 text-white" />
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="bg-white p-8 w-1/3 shadow-xl rounded-lg mt-10">
+            <form className="space-y-6">
+              <h2 className="text-center text-black font-semibold text-2xl">
+                Get in Touch
+              </h2>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-900 dark:text-black py-1">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="block w-full py-2 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-400 focus:outline-none focus:ring-0"
+                  placeholder="Enter Your Name"
+                  required={true}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-900 dark:text-black py-1">
+                  Institute
+                </label>
+                <input
+                  type="text"
+                  className="block w-full py-2 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-400 focus:outline-none focus:ring-0"
+                  placeholder="Enter Your Institute"
+                  required={true}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-900 dark:text-black py-1">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="block w-full py-2 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-400 focus:outline-none focus:ring-0"
+                  placeholder="Enter Your Email"
+                  required={true}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-900 dark:text-black py-1">
+                  Mobile
+                </label>
+                <input
+                  type="text"
+                  className="block w-full py-2 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-400 focus:outline-none focus:ring-0"
+                  placeholder="Enter Your Mobile"
+                  required={true}
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-900 dark:text-black py-1">
+                  Message
+                </label>
+                <textarea
+                  className="block w-full py-2 px-0 text-sm text-gray-900 bg-transparent border-b-2 border-gray-400 focus:outline-none focus:ring-0"
+                  placeholder="Enter Your Message"
+                  required={true}
+                ></textarea>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-md"
+                  type="submit"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
