41ef621e9b3c109f2bc7107723a428a8dca52771
Sanjay-mobrilz <sanjay@mobrilz.agency>
2025-01-27 12:41:42 +0530

    home_page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,8 +4,14 @@ import Layout from "../../UI/Layout/Layout";
 const Home = () => {
   return (
     <Layout>
-      <div className="flex justify-center items-center font-bold text-4xl h-screen text-black">
-        Home page
+      <div className="bg-blue-500 flex justify-start items-center h-screen px-9">
+       
+      <div className="text-white start">
+    <p className="text-3xl mb-2 font-semibold">Child Care</p>
+    <h1 className="text-7xl font-bold leading-tight">
+      School<br />Management<br />System
+    </h1>
+  </div>
       </div>
     </Layout>
   );
