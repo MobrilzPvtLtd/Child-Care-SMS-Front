35c1585cd4540a2c9d3417a0dcb1ffe01bf800df
Devmobrilz <devbrat@mobrilz.agency>
2025-03-21 16:30:50 +0530

    minor-change

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`, // Replace with your backend URL
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`, 
       },
     ];
   },
