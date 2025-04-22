abcf16ac6820039400ad79303ab3cb730847129a
Sanjay-mobrilz <sanjay@mobrilz.agency>
2025-04-22 17:16:10 +0530

    add onboard component

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -9,8 +9,16 @@ import Banner from './Banner'
 import Banner2 from './Banner2' 
 import Footer from '../common/Footer/Footer'
 import TabSection from './TabSection'
+import OnboardingForm from '../common/OnboardingForm'
 
 function Home() {
+  const customHeadings = {
+    step1: "First, tell us about yourself.",
+    step2: "What's your enrollment capacity?",
+    step3: "How long has your program been in business?",
+    step4: "What's your email address?",
+    step5: "Lastly, what's your contact information?",
+  };
   return (
     <> 
     <Banner/>
@@ -21,6 +29,12 @@ function Home() {
       <TestimonialsCarousel/>
      <FlowysisSection/> 
       {/* <RoleSelection/> */}
+      <div className="w-full flex flex-col items-center pt-6 text-center ">
+        <h1 className="text-5xl font-bold text-center leading-12  max-w-4xl">
+        Tour the easiest all-in-one childcare and preschool app
+        </h1>
+      </div>
+      <OnboardingForm showPurposeStep={false} headings={customHeadings} />
       <Footer/>
     </>
   )
