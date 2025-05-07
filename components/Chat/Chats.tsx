4eea8f166ac844f4822a260783ce6294a77f34ff
Devmobrilz <devbrat@mobrilz.agency>
2025-05-07 16:09:02 +0530

    BASE_URL updated

--- components/Chat/Chats.tsx
+++ components/Chat/Chats.tsx
@@ -57,7 +57,7 @@ const Chats: NextPage = () => {
 
   useEffect(() => {
     // Initialize Socket Connection
-    socketRef.current = io("http://localhost:3000", {
+    socketRef.current = io(`${process.env.BASE_URL}`, {
       withCredentials: true,
     });
 
